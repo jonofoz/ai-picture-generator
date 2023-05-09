d2e275015e45fadbf49dd1993ea64ed39b9a120d
JonOfOz <jon@jonofoz.com>
2023-05-08 22:21:41 -0500

    Make server.js file, some changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import * as dotenv from "dotenv";
-dotenv.config({path: "./.env.local"});
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
 
 import { Configuration, OpenAIApi } from "openai";
 const apiKey = process.env.API_KEY;
 
-if (!apiKey){
+if (!apiKey) {
     throw new Error("API_KEY is undefined.");
 }
 
@@ -22,24 +22,24 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/generate", async (req, res) => {
-    try{
+    try {
         const prompt = req.body.prompt;
 
         const aiResponse = await openAI.createImage({
-           prompt,
-           n: 1,
-           size: "1024x1024"
+            prompt,
+            n: 1,
+            size: "1024x1024"
         })
 
         const image = aiResponse.data.data[0].url
         res.send({ image })
     }
-    catch (error: any) {
+    catch (error) {
         console.error(error);
-        res.status(500).send(error?.response?.data?.error?.message || "Something went wrong" );
+        res.status(500).send(error?.response?.data?.error?.message || "Something went wrong");
     }
 });
 
 app.listen(8080, () => console.log("App is ready to generate art on 'http://localhost:8080/generate'!"))
 
-module.exports = app;
+export default app;
